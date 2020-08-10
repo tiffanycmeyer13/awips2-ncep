0547ee82520b198cacad68c495781abd51d2178d
Timothy Jensen <timothy.jensen@noaa.gov>
2020-08-10 18:00:33 -0500

    VLab Issue #81343 - Add missing hazard types to NonConvSigmetParser
    Change-Id: Id3effccd858b285d58b6de044d29f5f146578145

--- edex/gov.noaa.nws.ncep.edex.plugin.nonconvsigmet/src/gov/noaa/nws/ncep/edex/plugin/nonconvsigmet/util/NonConvSigmetParser.java
+++ edex/gov.noaa.nws.ncep.edex.plugin.nonconvsigmet/src/gov/noaa/nws/ncep/edex/plugin/nonconvsigmet/util/NonConvSigmetParser.java
@@ -1,28 +1,4 @@
-/*
- * Non-Convective Significant Meteorological Information DecoderUtil
- * 
- * This java class intends to serve as a decoder utility for NonConvsigmet.
- * 
- * HISTORY
- *
- * Date     	Author		Description
- * ------------	----------	-----------	--------------------------
- * 06/2009		Uma Josyula	Initial creation	
- * 07/2011		F. J. Yen	Fix for RTN TTR 9973--ConvSigment Decoder Ignoring
- * 							time range (NonConvsigmet, too).  Set the rangeEnd
- * 							time to the endTime
- * Jan 17, 2014         njensen         Handle if one or more locations not found in LatLonLocTbl	
- * May 14, 2014 2536    bclement        moved WMO Header to common, removed TimeTools usage						
- * 
- * This code has been developed by the SIB for use in the AWIPS2 system.
- */
-
 package gov.noaa.nws.ncep.edex.plugin.nonconvsigmet.util;
-
-import gov.noaa.nws.ncep.common.dataplugin.nonconvsigmet.NonConvSigmetLocation;
-import gov.noaa.nws.ncep.common.dataplugin.nonconvsigmet.NonConvSigmetRecord;
-import gov.noaa.nws.ncep.edex.tools.decoder.LatLonLocTbl;
-import gov.noaa.nws.ncep.edex.util.UtilN;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -38,6 +14,37 @@ import com.raytheon.uf.common.wmo.WMOHeader;
 import com.raytheon.uf.common.wmo.WMOTimeParser;
 import com.raytheon.uf.edex.decodertools.core.LatLonPoint;
 
+import gov.noaa.nws.ncep.common.dataplugin.nonconvsigmet.NonConvSigmetLocation;
+import gov.noaa.nws.ncep.common.dataplugin.nonconvsigmet.NonConvSigmetRecord;
+import gov.noaa.nws.ncep.edex.tools.decoder.LatLonLocTbl;
+import gov.noaa.nws.ncep.edex.util.UtilN;
+
+/**
+ *
+ * Non-Convective Significant Meteorological Information DecoderUtil
+ *
+ * This java class intends to serve as a decoder utility for NonConvsigmet.
+ *
+ * <pre>
+ *
+ * SOFTWARE HISTORY
+ *
+ * Date          Ticket#  Engineer     Description
+ * ------------- -------- ------------ -----------------------------------------
+ * 06/2009                Uma Josyula  Initial creation
+ * 07/2011                F. J. Yen    Fix for RTN TTR 9973--ConvSigment Decoder
+ *                                     Ignoring time range (NonConvsigmet, too).
+ *                                      Set the rangeEnd time to the endTime
+ * Jan 17, 2014           njensen      Handle if one or more locations not found
+ *                                     in LatLonLocTbl
+ * May 14, 2014  2536     bclement     moved WMO Header to common, removed
+ *                                     TimeTools usage
+ * Aug 10, 2020  81343    tjensen      Add missing hazard types
+ *
+ * </pre>
+ *
+ * @author Uma Josyula
+ */
 public class NonConvSigmetParser {
 
     /**
@@ -49,13 +56,14 @@ public class NonConvSigmetParser {
     /**
      * Parse the WMO line and store WMO header, OfficeID, issue time,
      * designatorBBB,...
-     * 
+     *
      * @param wmoline
      *            The bulletin message
-     * 
+     *
      * @return a NonConvsigmetRecord
      */
-    public static NonConvSigmetRecord processWMO(String wmoline, Headers headers) {
+    public static NonConvSigmetRecord processWMO(String wmoline,
+            Headers headers) {
 
         NonConvSigmetRecord currentRecord = null;
         // Regular expression for WMO/ICAO, station ID, and issue date (and
@@ -76,26 +84,21 @@ public class NonConvSigmetParser {
 
             // Decode the issue time.
             String fileName = (String) headers.get(WMOHeader.INGEST_FILE_NAME);
-            Calendar issueTime = WMOTimeParser.findDataTime(
-                    theMatcher.group(3), fileName);
+            Calendar issueTime = WMOTimeParser.findDataTime(theMatcher.group(3),
+                    fileName);
             currentRecord.setIssueTime(issueTime);
-
-            /* 999999999999999999999999999999
-            DataTime dataTime = new DataTime(issueTime);
-            currentRecord.setDataTime(dataTime);
-            999 */
         }
         return currentRecord;
     }
 
     /**
      * Obtains start time and end time and forecast Region from input report
-     * 
+     *
      * @param theBullMsg
      *            The bulletin message which contains start time and end time.
-     * 
+     *
      * @param NonConvSigmetRecord
-     * 
+     *
      * @return a NonConvsigmetRecord
      */
 
@@ -142,22 +145,20 @@ public class NonConvSigmetParser {
             currentRecord.setEndTime(endTime);
         }
 
-        /* 9999999999999999999999999999999 */
-        DataTime dataTime = new DataTime(stTime, new TimeRange (stTime.getTime(),
-        		endTime.getTimeInMillis() - stTime.getTimeInMillis()) );
+        DataTime dataTime = new DataTime(stTime, new TimeRange(stTime.getTime(),
+                endTime.getTimeInMillis() - stTime.getTimeInMillis()));
         currentRecord.setDataTime(dataTime);
-        /* 999 */
         return currentRecord;
     }
 
     /**
      * Obtains SigmetId from input report
-     * 
+     *
      * @param theBullMsg
      *            The bulletin message.
-     * 
+     *
      * @param NonConvSigmetRecord
-     * 
+     *
      * @return a NonConvsigmetRecord
      */
     public static NonConvSigmetRecord processSigmetId(String theBullMsg,
@@ -177,13 +178,13 @@ public class NonConvSigmetParser {
     /**
      * Parse the phenomena... Hazard Type,Hazard Intensity,Hazard Cause,Hazard
      * Condition,FlightLevel1,FlightLevel2,SigmetId,AWIPSId,CorAmdTest,StateList
-     * 
+     *
      * @param theBullMsg
      *            from bulletin message
-     * 
+     *
      * @param a
      *            NonConvsigmetRecord
-     * 
+     *
      * @return a NonConvsigmetRecord
      */
     public static NonConvSigmetRecord processPhenomena(String theBullMsg,
@@ -193,7 +194,7 @@ public class NonConvSigmetParser {
 
         final String FL_EXP2 = "BLW (FL)?([0-9]{3})";
 
-        final String HAZARDTYPE_EXP = " (TURB|ICGICIP|ICE) ";
+        final String HAZARDTYPE_EXP = " (TURB|ICGICIP|ICE|ICGIC|ICGIP|VA|DU) ";
 
         final String HAZARDINTS_EXP = "(OCNL [A-Z0-9]{2,})(BLW (FL)?([0-9]{3}).|BTN|TURB|ICGICIP|ICE|ICGIC|VA|DU)?";
 
@@ -237,8 +238,8 @@ public class NonConvSigmetParser {
         } else {
             theMatcher = flPattern2.matcher(theBullMsg);
             if (theMatcher.find()) {
-                currentRecord.setFlightLevel1(Integer.parseInt(theMatcher
-                        .group(2)));
+                currentRecord
+                        .setFlightLevel1(Integer.parseInt(theMatcher.group(2)));
             }
         }
         theMatcher = hzCausPattern.matcher(theBullMsg);
@@ -281,13 +282,13 @@ public class NonConvSigmetParser {
     /**
      * Parse the location lines... Add location table to the section table if
      * any
-     * 
+     *
      * @param theBullMsg
      *            from bulletin message
-     * 
+     *
      * @param recordTable
      *            The record Table
-     * 
+     *
      * @return true if finds a location line
      */
     public static Boolean processLocation(String theBullMsg,
@@ -295,7 +296,7 @@ public class NonConvSigmetParser {
 
         Boolean hasLocationLine = true;
         String locationDelimiter = "-| TO ";
-        ArrayList<String> terminationList = new ArrayList<String>();
+        ArrayList<String> terminationList = new ArrayList<>();
         terminationList.addAll(Arrays.asList(new String[] { "OCNL", "SEV" }));
         Scanner sclocations = new Scanner(theBullMsg).useDelimiter("FROM");
 
@@ -312,7 +313,7 @@ public class NonConvSigmetParser {
                     .useDelimiter("\\x0d\\x0d\\x0a");
             String lines = " ";
             String curLine = null;
-            ArrayList<String> locationList = new ArrayList<String>();
+            ArrayList<String> locationList = new ArrayList<>();
             locationList.clear();
             Boolean notBreak = true;
 
@@ -349,14 +350,14 @@ public class NonConvSigmetParser {
                     currentLocation.setLocationLine(lines);
                     currentLocation.setLocation(location);
                     point = LatLonLocTbl.getLatLonPoint(location, "vors");
-                    if(point != null) {
-                       currentLocation.setIndex(idxLocation + 1);
-                       idxLocation++;
-                       currentLocation.setLatitude(point
-                            .getLatitude(LatLonPoint.INDEGREES));
-                       currentLocation.setLongitude(point
-                            .getLongitude(LatLonPoint.INDEGREES));
-                       recordTable.addNonConvSigmetLocation(currentLocation);
+                    if (point != null) {
+                        currentLocation.setIndex(idxLocation + 1);
+                        idxLocation++;
+                        currentLocation.setLatitude(
+                                point.getLatitude(LatLonPoint.INDEGREES));
+                        currentLocation.setLongitude(
+                                point.getLongitude(LatLonPoint.INDEGREES));
+                        recordTable.addNonConvSigmetLocation(currentLocation);
                     }
                 }
                 hasLocationLine = (idxLocation > 0);
@@ -373,7 +374,7 @@ public class NonConvSigmetParser {
 
     /**
      * process regular section of a non-convective sigmet report
-     * 
+     *
      * @return a NonConvsigmetRecord table
      */
     public static NonConvSigmetRecord processRecord(String theBullMsg,
@@ -382,8 +383,8 @@ public class NonConvSigmetParser {
         final Pattern cancelSigPattern = Pattern.compile(CANCELSIGMET_EXP);
         Matcher theMatcher = cancelSigPattern.matcher(theBullMsg);
         // Decode the WMO Header
-        NonConvSigmetRecord currentRecord = NonConvSigmetParser.processWMO(
-                theBullMsg, headers);
+        NonConvSigmetRecord currentRecord = NonConvSigmetParser
+                .processWMO(theBullMsg, headers);
         // Decode the sigmetId
         currentRecord = NonConvSigmetParser.processSigmetId(theBullMsg,
                 currentRecord);
@@ -394,10 +395,9 @@ public class NonConvSigmetParser {
         currentRecord = NonConvSigmetParser.processPhenomena(theBullMsg,
                 currentRecord);
         // Replace the special characters
-        currentRecord
-                .setBullMessage(theBullMsg.replace('\r', ' ')
-                        .replace('\003', ' ').replace('\000', ' ')
-                        .replace('\001', ' '));
+        currentRecord.setBullMessage(
+                theBullMsg.replace('\r', ' ').replace('\003', ' ')
+                        .replace('\000', ' ').replace('\001', ' '));
 
         if (theMatcher.find()) {
             if ("UNKNOWN".equals(currentRecord.getHazardType())) {
